9f880db177cbf82dd5a8f75807d438e372706306
ruthosoro <robinahruth@gmail.com>
2023-08-15 09:39:19 -0700

    print

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for integer in row:
+                if integer == row[-1]:
+                    print("{:d}".format(integer), end="")
+                else:
+                    print("{:d}".format(integer), end=" ")
+            print()
