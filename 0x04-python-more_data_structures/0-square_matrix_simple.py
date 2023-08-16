320e50d2383a8966ac0a852b6d6b084ce9221533
ruthosoro <robinahruth@gmail.com>
2023-08-16 12:13:19 -0700

    sq

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        matrix_copy = [row[:] for row in matrix]
-        matrix_square = list(map(
-            lambda row: list(map(lambda x: x**2, row)), matrix_copy
-            ))
-        return matrix_square
+    if len(matrix) == 0:
+        return matrix
+    squared_matrix = list(map(lambda row: list(map(lambda x: x * x, row)),
+                              matrix))
+    return squared_matrix
