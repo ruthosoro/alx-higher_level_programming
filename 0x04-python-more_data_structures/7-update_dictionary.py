526547823ecb3b4bb12bc62b68955111afcd60b7
ruthosoro <robinahruth@gmail.com>
2023-08-16 12:33:08 -0700

    update

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary:
+        if key in a_dictionary:
+            a_dictionary[key] = value
+        elif key not in a_dictionary:
+            a_dictionary[key] = value
+        return a_dictionary
+    return None
