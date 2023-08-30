4c451bf73ec7a2f7530b815a2b1a16e24e618b8b
ruthosoro <robinahruth@gmail.com>
2023-08-30 01:33:00 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This function defines a class Square """
+
+
+class Square:
+    """This is an class square containing size"""
+
+    def __init__(self, __size):
+        """Initialising the instance variable"""
+
+        self.__size = __size
