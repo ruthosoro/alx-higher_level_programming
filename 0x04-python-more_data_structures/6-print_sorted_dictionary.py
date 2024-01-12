ffee6b0ffc0b6ba9c2f5e58b3378a9669625895f
ruthosoro <robinahruth@gmail.com>
2024-01-12 10:46:23 +0300

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = sorted(list(a_dictionary.keys()))
+    for item in keys:
+        print("{0}: {1}".format(item, a_dictionary.get(item)))
