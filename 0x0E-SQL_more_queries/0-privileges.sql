4467d57d5ae68d14fe9a20f70bb09ab2f6377fbc
ruthosoro <robinahruth@gmail.com>
2024-03-14 12:03:34 +0300

    priv

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Lists privileges of Mysql users user_0d_1 and user_0d_2
--- On a localhost
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
